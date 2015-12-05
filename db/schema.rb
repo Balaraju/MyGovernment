22b0c303985d04884bd1e60f5bdef810c74496b7
Balaraju <vankalabalaraju@gmail.com>
2015-12-05 14:52:20 +0530

    Balaraju - Added Services

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151205075650) do
+ActiveRecord::Schema.define(version: 20151205091010) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 20151205075650) do
     t.integer  "district_id", limit: 4
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string   "name",          limit: 255
+    t.text     "description",   limit: 65535
+    t.integer  "department_id", limit: 4
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
   create_table "states", force: :cascade do |t|
