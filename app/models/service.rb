ca3b0f1262bf2835f0c170f4850bd179b2311f53
Balaraju <vankalabalaraju@gmail.com>
2015-12-05 20:05:05 +0530

    Balaraju - Added Code For Excel

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,7 +3,7 @@ require 'elasticsearch/model'
 class Service < ActiveRecord::Base
 
 	include Elasticsearch::Model
-    include Elasticsearch::Model::Callbacks
+  include Elasticsearch::Model::Callbacks
 
 	validates :name, presence: true
 	belongs_to :department
